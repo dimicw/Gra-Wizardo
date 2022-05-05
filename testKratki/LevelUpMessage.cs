d00fe5e5d48e60f7e50c863832f8c3c9db0cb663
UnFriend-PL <104201558+UnFriend-PL@users.noreply.github.com>
2022-05-05 21:03:17 +0200

    Klasy_w_osobnych_plikach
    Klasy w osobnych plikach,
    Schemat UML

---
+++ testKratki/LevelUpMessage.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testKratki
+{
+	public partial class LevelUpMessage : Form                  // form for level ups
+    {
+		public LevelUpMessage()
+		{
+			Text = "Level Up!";
+			Size = new Size(640, 128);
+
+			Label info = new Label();
+			Controls.Add(info);
+			info.Size = new Size(200, 32);
+			info.Location = new Point(220, 10);
+			info.Text = "Choose Upgrade!";
+			info.Font = new Font(info.Font.FontFamily, 16, info.Font.Style);
+
+			Button buttonMoreHP = addCustomButton("More HP", 128, 32, 21, 50);
+			Button buttonManaRegen = addCustomButton("Mana Regeneration", 128, 32, 171, 50);
+			Button buttonAddSpell2 = addCustomButton("Arcane Bolt", 128, 32, 321, 50);
+			Button buttonAddSpell3 = addCustomButton("Circle of Protection", 128, 32, 471, 50);
+
+			if (Values.player.spell2unlocked) buttonAddSpell2.Enabled = false;
+
+			if (Values.player.spell3unlocked) buttonAddSpell3.Enabled = false;
+
+			buttonMoreHP.Click += new EventHandler(buttonMoreHPClicked);
+			buttonManaRegen.Click += new EventHandler(buttonManaRegenClicked);
+			buttonAddSpell2.Click += new EventHandler(buttonAddSpell2Clicked);
+			buttonAddSpell3.Click += new EventHandler(buttonAddSpell3Clicked);
+		}
+
+		private Button addCustomButton(string text, int width, int height, int positionX, int positionY)    // create new button with custom paramenters
+		{
+			Button button1 = new Button();
+			Controls.Add(button1);
+			button1.Text = text;
+			button1.Size = new Size(width, height);
+			button1.Location = new Point(positionX, positionY);
+
+			return button1;
+		}
+
+		protected void buttonMoreHPClicked(object sender, EventArgs e)
+		{
+			Values.player.maxHP += 15;              // increase maximum HP by 15 points
+			Close();
+		}
+		protected void buttonManaRegenClicked(object sender, EventArgs e)
+		{
+			Values.player.manaRegen += 3;           // increase mana regeneration by 3 points
+			Close();
+		}
+		protected void buttonAddSpell2Clicked(object sender, EventArgs e)
+		{
+			Values.player.spell2unlocked = true;    // unlock Arcane Bolt
+			Close();
+		}
+		protected void buttonAddSpell3Clicked(object sender, EventArgs e)
+		{
+			Values.player.spell3unlocked = true;    // unlock Circle of Protection
+			Close();
+		}
+
+
+	}
+
+}
---
+++ testKratki/Zombie.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testKratki
+{
+	public class Zombie : Creature                          // class for zombies
+	{
+		int dmg;                                            // damage dealt with each attack
+		public bool alive;                                  // true if it can move and attack
+
+		public Zombie(int positionY, int positionX)     // function to create a new zombie and assingn custom location
+		{
+			HP = 10;
+			dmg = 2;
+			alive = true;
+
+			facing = 0;
+			movement = true;
+			attack = true;
+
+			this.positionX = positionX;
+			this.positionY = positionY;
+			previousPositionX = positionX;
+			previousPositionY = positionY;
+
+			Values.occupiedTile[positionY, positionX] = true;
+		}
+
+		public void brainless()                                                 // zombie movement and attack
+		{
+			int xDifference = Values.player.positionX - positionX;              // the difference between the player and the zombie on x axis
+			int yDifference = Values.player.positionY - positionY;              // the difference between the player and the zombie on y axis
+
+			if (Math.Abs(xDifference) <= 1 && Math.Abs(yDifference) <= 1)       // check if the zombie is near the player, so it can attack 
+			{
+				if (Values.player.HP > dmg) Values.player.HP -= dmg;            // check if the hp will be positive after the damage
+				else
+				{
+					Values.player.HP = 0;                                       // change to 0 instead of negative integer
+					MessageBox.Show("You Died!");                               // information of the failiure
+					Application.Exit();                                         // close the game
+				}
+			}
+			else                                                                // if it can't attack, it moves to the player
+			{
+				if (xDifference > 0 && (positionX != Values.xAxis - 1 && Values.occupiedTile[positionY, positionX + 1] == false)) move(0, 1);
+				else if (xDifference < 0 && (positionX != 0 && Values.occupiedTile[positionY, positionX - 1] == false)) move(0, -1);
+				else if (yDifference > 0 && (positionY != Values.yAxis - 1 && Values.occupiedTile[positionY + 1, positionX] == false)) move(1, 0);
+				else if (yDifference < 0 && (positionY != 0 && Values.occupiedTile[positionY - 1, positionX] == false)) move(-1, 0);
+				else
+				{
+					switch (new Random().Next(4))                               // if it cant find a direct path to the player, it wonders in random direction
+					{
+						case 0:
+							if (positionX != Values.xAxis - 1 && Values.occupiedTile[positionY, positionX + 1] == false) move(0, 1);
+							break;
+						case 1:
+							if (positionX != 0 && Values.occupiedTile[positionY, positionX - 1] == false) move(0, -1);
+							break;
+						case 2:
+							if (positionY != Values.yAxis - 1 && Values.occupiedTile[positionY + 1, positionX] == false) move(1, 0);
+							break;
+						case 3:
+							if (positionY != 0 && Values.occupiedTile[positionY - 1, positionX] == false) move(-1, 0);
+							break;
+						default:
+							break;
+					}
+				}
+			}
+		}
+
+		private void move(int y, int x)
+		{
+			previousPositionX = positionX;                                                          // save current position
+			previousPositionY = positionY;
+
+			positionX += x;                                                                         // move to the new tile
+			positionY += y;
+
+			Values.board[previousPositionY, previousPositionX].Image = null;                        // clear the previous tile
+
+			if (x > 0) Values.board[positionY, positionX].Image = Values.zombieEastImage;           // place and rotate zombie on the new tile
+			else if (x < 0) Values.board[positionY, positionX].Image = Values.zombieWestImage;
+			else if (y > 0) Values.board[positionY, positionX].Image = Values.zombieSouthImage;
+			else if (y < 0) Values.board[positionY, positionX].Image = Values.zombieNorthImage;
+
+			Values.occupiedTile[previousPositionY, previousPositionX] = false;                      // make the previous tile available
+			Values.occupiedTile[positionY, positionX] = true;                                       // make the new tile unavailable
+
+		}
+	}
+}
